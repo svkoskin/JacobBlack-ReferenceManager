9eb3ae86edcc0f7c14a23e089e4ce10f72d76afe
Joosa Kurvinen <joosa.kurvinen@helsinki.fi>
2012-03-31 13:19:49 +0300

    validationObj feature 1. commit

--- miniprojekti/src/main/java/ohtu/miniprojekti/domain/Viite.java
+++ miniprojekti/src/main/java/ohtu/miniprojekti/domain/Viite.java
@@ -5,27 +5,47 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import ohtu.miniprojekti.formvalidation.ArticleValidationObject;
 
 @Entity
 public class Viite implements Serializable {
+    public enum ViiteType {ARTICLE, BOOK, INPROCEEDINGS}
+    
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
-    @NotNull(message="Viitteellä täytyy olla otsikko.")
-    @Size(min=1, max=20, message="Otsikon pituus 1-20 merkkiä.")
-    @Pattern(regexp="[\\w äöÄÖ]+", message="Virheellinen nimi.")
+    private ViiteType viiteType;
+    private String author;
     private String title;
+    private String journal;
+    private String volume;
+    private String number;
+    private String year;
+    private String pages;
+    private String publisher;
+    
 
-    public String getTitle() {
-        return title;
+    public Viite() {
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public Viite(ArticleValidationObject validationObject) {
+        this.viiteType = ViiteType.ARTICLE;
+        this.author = validationObject.getAuthor();
+        this.title = validationObject.getTitle();
+        this.journal = validationObject.getJournal();
+        this.volume = validationObject.getVolume();
+        this.number = validationObject.getNumber();
+        this.year = validationObject.getYear();
+        this.pages = validationObject.getPages();
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public Long getId() {
@@ -35,5 +55,71 @@ public class Viite implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ViiteType getViiteType() {
+        return viiteType;
+    }
+
+    public void setViiteType(ViiteType viiteType) {
+        this.viiteType = viiteType;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    
     
 }
--- miniprojekti/src/main/webapp/WEB-INF/view/artikkeli.jsp
+++ miniprojekti/src/main/webapp/WEB-INF/view/artikkeli.jsp
@@ -9,9 +9,17 @@
     <body>
         <h1>Uusi viite (artikkeli):</h1>
         <form:form commandName="viite" action="artikkeli" method="POST">
-            Title: <form:input path="title" /> <form:errors path="title" /><br/>
+            *Tekijät: <form:input path="author" /> <form:errors path="author" /><br/>
+            *Otsikko: <form:input path="title" /> <form:errors path="title" /><br/>
+            *Journal: <form:input path="journal" /> <form:errors path="journal" /><br/>
+            *Vuosi: <form:input path="year" /> <form:errors path="year" /><br/>
+            Vuosikerta: <form:input path="volume" /> <form:errors path="volume" /><br/>
+            Numero: <form:input path="number" /> <form:errors path="number" /><br/>
+            Sivut: <form:input path="pages" /> <form:errors path="pages" /><br/>
             <input type="submit" value="Lähetä" />
         </form:form>   
+        <br/>
+        <h4>(*) pakollinen kenttä</h4>
         
     </body>
 </html>
